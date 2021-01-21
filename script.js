68530cbfe6812c0a46ea4f46c637943c6604c1b9
Aislanio <aislanioamorim@gmail.com>
2021-01-21 11:50:08 -0300

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-const email = document.querySelector("#email");
-const password = document.querySelector("#Password");
 
+document.querySelector('form input').oninvalid = function() {  
+    // remove mensagens de erro padrão
+    document.querySelector("article").style.display = "flex"
+    // faz a validação novamente
+    if (!this.validity.valid) {
+        // se estiver inválido, coloca a mensagem
+        this.setCustomValidity("Fill this field");
+        
+    }
+ };
+document.querySelector("#ok").addEventListener("click", () => {
+    document.querySelector("article").style.display = "none"
+});
